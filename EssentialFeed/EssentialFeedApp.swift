03ea4e5673d89f4d940c7324d91f0c62d7ec7616
Jidh George <jidh.george2@sky.uk>
2022-01-03 14:24:01 +0000

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Jidh George on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
